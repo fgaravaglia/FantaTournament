210fb381558c74b9ae3d62c11f78cd0cfcc79612
Francesco Garavaglia <Francesco.Garavaglia@outlook.it>
2026-02-01 00:57:56 +0100

    Cleanup

--- src/Tests/FantaTournament.Domain.Tests/TestCategories.cs
+++
@@ -1,9 +0,0 @@
-namespace FantaTournament.Domain.Tests
-{
-    public static class TestCategories
-    {
-        public const string UNIT = "UNIT";
-
-        public const string INTEGRATION = "INTEGRATION";
-    }
-}
